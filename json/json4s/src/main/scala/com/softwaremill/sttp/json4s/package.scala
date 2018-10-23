2fd3158b7e4aa58d6c06fa88e37e3518efc7eb16
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2018-10-23 18:14:21 +0200

    Depend on json4s interface instead of native impl

--- json/json4s/src/main/scala/com/softwaremill/sttp/json4s/package.scala
+++ json/json4s/src/main/scala/com/softwaremill/sttp/json4s/package.scala
@@ -3,12 +3,12 @@ package com.softwaremill.sttp
 import com.softwaremill.sttp.internal._
 
 import org.json4s._
-import org.json4s.native.Serialization.{read, write}
+import org.json4s.Serialization
 
 package object json4s {
-  implicit def json4sBodySerializer[B <: AnyRef](implicit formats: Formats = DefaultFormats): BodySerializer[B] =
-    b => StringBody(write(b), Utf8, Some(MediaTypes.Json))
+  implicit def json4sBodySerializer[B <: AnyRef](implicit formats: Formats = DefaultFormats, serialization: Serialization): BodySerializer[B] =
+    b => StringBody(serialization.write(b), Utf8, Some(MediaTypes.Json))
 
-  def asJson[B: Manifest](implicit formats: Formats = DefaultFormats): ResponseAs[B, Nothing] =
-    asString(Utf8).map(s => read[B](s))
+  def asJson[B: Manifest](implicit formats: Formats = DefaultFormats, serialization: Serialization): ResponseAs[B, Nothing] =
+    asString(Utf8).map(s => serialization.read[B](s))
 }
