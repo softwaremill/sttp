0e0f556b455c1f177a779c20a8d3b0931f918551
adamw <adamw@warski.org>
2018-10-24 17:13:00 +0200

    Format code

--- json/json4s/src/main/scala/com/softwaremill/sttp/json4s/package.scala
+++ json/json4s/src/main/scala/com/softwaremill/sttp/json4s/package.scala
@@ -6,9 +6,11 @@ import org.json4s._
 import org.json4s.Serialization
 
 package object json4s {
-  implicit def json4sBodySerializer[B <: AnyRef](implicit formats: Formats = DefaultFormats, serialization: Serialization): BodySerializer[B] =
+  implicit def json4sBodySerializer[B <: AnyRef](implicit formats: Formats = DefaultFormats,
+                                                 serialization: Serialization): BodySerializer[B] =
     b => StringBody(serialization.write(b), Utf8, Some(MediaTypes.Json))
 
-  def asJson[B: Manifest](implicit formats: Formats = DefaultFormats, serialization: Serialization): ResponseAs[B, Nothing] =
+  def asJson[B: Manifest](implicit formats: Formats = DefaultFormats,
+                          serialization: Serialization): ResponseAs[B, Nothing] =
     asString(Utf8).map(s => serialization.read[B](s))
 }
