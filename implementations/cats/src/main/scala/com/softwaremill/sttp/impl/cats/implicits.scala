9c843c3da37ec595024705c4d31c8c3d89df68fb
Jakub Kozłowski <kubukoz@gmail.com>
2019-04-03 22:41:53 +0200

    Split out syntax traits

--- implementations/cats/src/main/scala/com/softwaremill/sttp/impl/cats/implicits.scala
+++ implementations/cats/src/main/scala/com/softwaremill/sttp/impl/cats/implicits.scala
@@ -5,16 +5,24 @@ import cats.~>
 
 import scala.language.higherKinds
 
-object implicits {
-  implicit class MappableSttpBackend[R[_], S](val sttpBackend: SttpBackend[R, S]) extends AnyVal {
-    def mapK[G[_]: MonadError](f: R ~> G): SttpBackend[G, S] = new MappedKSttpBackend(sttpBackend, f, implicitly)
-  }
+object implicits extends CatsImplicits
+
+trait CatsImplicits {
+  implicit def sttpBackendToCatsMappableSttpBackend[R[_], S](
+      sttpBackend: SttpBackend[R, S]
+  ): MappableSttpBackend[R, S] = new MappableSttpBackend(sttpBackend)
 }
 
-private[cats] final class MappedKSttpBackend[F[_], -S, G[_]](wrapped: SttpBackend[F, S],
-                                                             mapping: F ~> G,
-                                                             val responseMonad: MonadError[G])
-    extends SttpBackend[G, S] {
+class MappableSttpBackend[R[_], S] private[cats] (val sttpBackend: SttpBackend[R, S]) extends AnyVal {
+  def mapK[G[_]: MonadError](f: R ~> G): SttpBackend[G, S] =
+    new MappedKSttpBackend(sttpBackend, f, implicitly)
+}
+
+private[cats] final class MappedKSttpBackend[F[_], -S, G[_]](
+    wrapped: SttpBackend[F, S],
+    mapping: F ~> G,
+    val responseMonad: MonadError[G]
+) extends SttpBackend[G, S] {
   def send[T](request: Request[T, S]): G[Response[T]] = mapping(wrapped.send(request))
 
   def close(): Unit = wrapped.close()
--- implementations/scalaz/src/main/scala/com/softwaremill/sttp/impl/scalaz/implicits.scala
+++ implementations/scalaz/src/main/scala/com/softwaremill/sttp/impl/scalaz/implicits.scala
@@ -5,16 +5,24 @@ import scalaz.~>
 
 import scala.language.higherKinds
 
-object implicits {
-  implicit class MappableSttpBackend[R[_], S](val sttpBackend: SttpBackend[R, S]) extends AnyVal {
-    def mapK[G[_]: MonadError](f: R ~> G): SttpBackend[G, S] = new MappedKSttpBackend(sttpBackend, f, implicitly)
-  }
+object implicits extends ScalazImplicits
+
+trait ScalazImplicits {
+  implicit def sttpBackendToScalazMappableSttpBackend[R[_], S](
+      sttpBackend: SttpBackend[R, S]
+  ): MappableSttpBackend[R, S] = new MappableSttpBackend(sttpBackend)
 }
 
-private[scalaz] final class MappedKSttpBackend[F[_], -S, G[_]](wrapped: SttpBackend[F, S],
-                                                               mapping: F ~> G,
-                                                               val responseMonad: MonadError[G])
-    extends SttpBackend[G, S] {
+class MappableSttpBackend[R[_], S] private[scalaz] (val sttpBackend: SttpBackend[R, S]) extends AnyVal {
+  def mapK[G[_]: MonadError](f: R ~> G): SttpBackend[G, S] =
+    new MappedKSttpBackend(sttpBackend, f, implicitly)
+}
+
+private[scalaz] final class MappedKSttpBackend[F[_], -S, G[_]](
+    wrapped: SttpBackend[F, S],
+    mapping: F ~> G,
+    val responseMonad: MonadError[G]
+) extends SttpBackend[G, S] {
   def send[T](request: Request[T, S]): G[Response[T]] = mapping(wrapped.send(request))
 
   def close(): Unit = wrapped.close()
