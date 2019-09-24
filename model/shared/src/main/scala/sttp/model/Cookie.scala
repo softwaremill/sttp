5ff4089c3a8ee40bb80e0a9348b24bc8acc2e9fa
adamw <adam@warski.org>
2019-09-24 18:14:34 +0200

    Comment

--- model/shared/src/main/scala/sttp/model/Cookie.scala
+++ model/shared/src/main/scala/sttp/model/Cookie.scala
@@ -44,6 +44,7 @@ object Cookie {
       httpOnly: Boolean = false
   ): Cookie = Cookie(CookiePair(name, value), expires, maxAge, domain, path, secure, httpOnly)
 
+  // https://tools.ietf.org/html/rfc6265#section-4.1.1
   def parseHeaderValue(s: String): Either[String, Cookie] = {
     def splitkv(kv: String): (String, Option[String]) = kv.split("=", 2).map(_.trim) match {
       case Array(v1)     => (v1, None)
