4f5e4ceff8d86046a6abf50bb32b7421e5643a43
adamw <adam@warski.org>
2019-10-01 20:37:25 +0200

    Fix test

--- core/shared/src/test/scala/sttp/client/testing/SttpBackendStubTests.scala
+++ core/shared/src/test/scala/sttp/client/testing/SttpBackendStubTests.scala
@@ -115,7 +115,7 @@ class SttpBackendStubTests extends FlatSpec with Matchers with ScalaFutures {
       .thenRespond(throw new TimeoutException())
 
     val result = basicRequest.get(uri"http://example.org").send()
-    result.failed.futureValue shouldBe a[TimeoutException]
+    result.failed.map(_ shouldBe a[TimeoutException])
   }
 
   it should "try to convert a basic response to a mapped one" in {
