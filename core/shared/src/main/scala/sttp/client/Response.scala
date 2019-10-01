3b2351ba3c761882677f11c7d594ec2972a286e5
adamw <adam@warski.org>
2019-10-01 15:36:50 +0200

    Add a response constructor for testing

--- core/shared/src/main/scala/sttp/client/Response.scala
+++ core/shared/src/main/scala/sttp/client/Response.scala
@@ -27,6 +27,13 @@ object Response {
     * Convenience method to create a Response instance, mainly useful in tests using
     * [[com.softwaremill.sttp.testing.SttpBackendStub]] and partial matchers.
     */
+  def apply[T](body: T, code: StatusCode): Response[T] =
+    Response(body, code, "", Nil, Nil)
+
+  /**
+    * Convenience method to create a Response instance, mainly useful in tests using
+    * [[com.softwaremill.sttp.testing.SttpBackendStub]] and partial matchers.
+    */
   def apply[T](body: T, code: StatusCode, statusText: String): Response[T] =
     Response(body, code, statusText, Nil, Nil)
 
