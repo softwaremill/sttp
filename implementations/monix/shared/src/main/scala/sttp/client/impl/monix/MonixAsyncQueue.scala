7dfea37cd72fb3024be14840635a1e72a9df6bd3
ghostbuster91 <kghost0@gmail.com>
2019-10-25 10:16:15 +0200

    Fix shadowed names

--- implementations/monix/shared/src/main/scala/sttp/client/impl/monix/MonixAsyncQueue.scala
+++ implementations/monix/shared/src/main/scala/sttp/client/impl/monix/MonixAsyncQueue.scala
@@ -2,13 +2,13 @@ package sttp.client.impl.monix
 
 import monix.eval.Task
 import sttp.model.ws.WebSocketBufferFull
-import monix.execution.{Scheduler, AsyncQueue => MonixAsyncQueue}
+import monix.execution.{Scheduler, AsyncQueue => MAsyncQueue}
 import sttp.client.ws.internal.AsyncQueue
 
 class MonixAsyncQueue[A](bufferCapacity: Option[Int])(implicit s: Scheduler) extends AsyncQueue[Task, A] {
   private val queue = bufferCapacity match {
-    case Some(capacity) => MonixAsyncQueue.bounded[A](capacity)
-    case None           => MonixAsyncQueue.unbounded[A]()
+    case Some(capacity) => MAsyncQueue.bounded[A](capacity)
+    case None           => MAsyncQueue.unbounded[A]()
   }
 
   override def clear(): Unit = queue.clear()
