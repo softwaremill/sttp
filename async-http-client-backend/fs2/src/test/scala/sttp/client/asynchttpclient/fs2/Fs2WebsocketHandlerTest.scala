0c2542e057ebe62f44e1926938754de7fb54a4d7
Yannick Heiber <yannick.heiber@carjump.de>
2019-10-27 19:39:24 +0100

    Add websocket support to async-http-client fs2 backend

---
+++ async-http-client-backend/fs2/src/test/scala/sttp/client/asynchttpclient/fs2/Fs2WebsocketHandlerTest.scala
@@ -0,0 +1,28 @@
+package sttp.client.asynchttpclient.fs2
+
+import cats.effect.{ContextShift, IO, Timer}
+import sttp.client._
+import sttp.client.asynchttpclient.{WebSocketHandler, WebsocketHandlerTest}
+import sttp.client.impl.cats.CatsMonadAsyncError
+import sttp.client.monad.MonadError
+import sttp.client.testing.ConvertToFuture
+import sttp.client.ws.WebSocket
+
+import scala.concurrent.Future
+
+class Fs2WebsocketHandlerTest extends WebsocketHandlerTest[IO] {
+  implicit val backend: SttpBackend[IO, Nothing, WebSocketHandler] = AsyncHttpClientFs2Backend[IO]().unsafeRunSync()
+  implicit val convertToFuture: ConvertToFuture[IO] = new ConvertToFuture[IO] {
+    override def toFuture[T](value: IO[T]): Future[T] = value.unsafeToFuture()
+  }
+  override implicit val monad: MonadError[IO] = new CatsMonadAsyncError[IO]
+  implicit lazy val contextShift: ContextShift[IO] = IO.contextShift(implicitly)
+  implicit lazy val timer: Timer[IO] = IO.timer(implicitly)
+
+  override def createHandler: Option[Int] => WebSocketHandler[WebSocket[IO]] = Fs2WebSocketHandler[IO](_).unsafeRunSync()
+
+  override protected def afterAll(): Unit = {
+    backend.close().toFuture
+    super.afterAll()
+  }
+}
