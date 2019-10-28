d9d5a3fe13bb51cdccd515d64537a75961f0ceaa
ghostbuster91 <kghost0@gmail.com>
2019-10-28 14:10:16 +0100

    Create opentracing backend

---
+++ metrics/open-tracing-backend/src/main/scala/sttp/client/opentracing/OpenTracingBackend.scala
@@ -0,0 +1,74 @@
+package sttp.client.opentracing
+
+import io.opentracing.tag.Tags
+import io.opentracing.Tracer
+import io.opentracing.propagation.Format
+import sttp.client.monad.MonadError
+import sttp.client.ws.WebSocketResponse
+import sttp.client.{FollowRedirectsBackend, NothingT, Request, Response, SttpBackend}
+import sttp.client.monad.syntax._
+import scala.collection.JavaConverters._
+
+class OpenTracingBackend[F[_], S] private (delegate: SttpBackend[F, S, NothingT], tracer: Tracer)
+    extends SttpBackend[F, S, NothingT] {
+
+  private implicit val _monad: MonadError[F] = responseMonad
+
+  override def send[T](request: Request[T, S]): F[Response[T]] =
+    responseMonad
+      .eval {
+        tracer
+          .buildSpan(
+            request
+              .tag(OpenTracingBackend.OperationIdRequestTag)
+              .getOrElse("default-operation-id")
+              .toString
+          )
+          .withTag(Tags.SPAN_KIND, Tags.SPAN_KIND_CLIENT)
+          .withTag(Tags.HTTP_METHOD, request.method.method)
+          .withTag(Tags.HTTP_URL, request.uri.toString)
+          .withTag(Tags.COMPONENT, "opentracing-sttp-client")
+          .start()
+      }
+      .flatMap { span =>
+        val requestBuilderAdapter = new RequestBuilderAdapter(request)
+        tracer.inject(span.context(), Format.Builtin.HTTP_HEADERS, new RequestBuilderCarrier(requestBuilderAdapter))
+        responseMonad.handleError(
+          delegate.send(requestBuilderAdapter.request).map { response =>
+            span
+              .setTag(Tags.HTTP_STATUS, Integer.valueOf(response.code.code))
+              .finish()
+            response
+          }
+        ) {
+          case e =>
+            span
+              .setTag(Tags.ERROR, java.lang.Boolean.TRUE)
+              .log(Map("event" -> Tags.ERROR.getKey, "error.object" -> e).asJava)
+              .finish()
+            responseMonad.error(e)
+        }
+      }
+
+  override def openWebsocket[T, WS_RESULT](
+      request: Request[T, S],
+      handler: NothingT[WS_RESULT]
+  ): F[WebSocketResponse[WS_RESULT]] = handler
+
+  override def close(): F[Unit] = delegate.close()
+
+  override def responseMonad: MonadError[F] = delegate.responseMonad
+}
+
+object OpenTracingBackend {
+  private val OperationIdRequestTag = "io.opentracing.tag.sttp.operationId"
+
+  implicit class RichRequest[T, S](request: Request[T, S]) {
+    def tagWithOperationId(operationId: String): Request[T, S] =
+      request.tag(OperationIdRequestTag, operationId)
+  }
+
+  def apply[F[_], S](delegate: SttpBackend[F, S, NothingT], tracer: Tracer): SttpBackend[F, S, NothingT] = {
+    new FollowRedirectsBackend[F, S, NothingT](new OpenTracingBackend(delegate, tracer))
+  }
+}
---
+++ metrics/open-tracing-backend/src/test/scala/sttp/client/opentracing/OpenTracingBackendTest.scala
@@ -0,0 +1,113 @@
+package sttp.client.opentracing
+
+import io.opentracing.mock.MockTracer
+import io.opentracing.tag.Tags
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import sttp.client.monad.IdMonad
+import sttp.client.testing.SttpBackendStub
+import sttp.client.{Identity, NothingT, SttpBackend, _}
+import sttp.model.{Method, StatusCode}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.util.Try
+
+class OpenTracingBackendTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  private val recordedRequests = mutable.MutableList[Request[_, _]]()
+  private val tracer = new MockTracer()
+  private implicit val backend: SttpBackend[Identity, Nothing, NothingT] =
+    OpenTracingBackend[Identity, Nothing](
+      SttpBackendStub.apply(IdMonad).whenRequestMatchesPartial {
+        case r if r.uri.toString.contains("echo") =>
+          recordedRequests += r
+          Response.ok("")
+        case r if r.uri.toString.contains("error") =>
+          throw new RuntimeException("something went wrong")
+      },
+      tracer
+    )
+
+  before {
+    recordedRequests.clear()
+    tracer.reset()
+  }
+
+  "OpenTracingBackendTest" should "propagate span" in {
+    val response = basicRequest.post(uri"http://stub/echo").send()
+    response.code shouldBe StatusCode.Ok
+
+    val spans = tracer.finishedSpans()
+    spans should have size 1
+    val spanId = spans.asScala.head.context().spanId()
+    val traceId = spans.asScala.head.context().traceId()
+    recordedRequests(0).headers.collectFirst { case h if h.name == "spanid"  => h.value } shouldBe Some(spanId.toString)
+    recordedRequests(0).headers.collectFirst { case h if h.name == "traceid" => h.value } shouldBe Some(
+      traceId.toString
+    )
+  }
+
+  it should "make child of current span" in {
+    val operationName = "my-custom-ops-id"
+    val span = tracer.buildSpan(operationName).start()
+    tracer.activateSpan(span)
+    val response = basicRequest.post(uri"http://stub/echo").send()
+    response.code shouldBe StatusCode.Ok
+    span.finish()
+
+    val spans = tracer.finishedSpans()
+    spans should have size 2
+    val spanId = span.context().spanId()
+    spans.asScala(0).parentId() shouldBe spanId
+    spans.asScala(1).context().spanId() shouldBe spanId
+    spans.asScala(1).operationName() shouldBe operationName
+  }
+
+  it should "propagate additional metadata" in {
+    val span = tracer.buildSpan("my-custom-ops-id").start()
+    span.setBaggageItem("baggage1", "hello")
+    tracer.activateSpan(span)
+    val response = basicRequest.post(uri"http://stub/echo").send()
+    response.code shouldBe StatusCode.Ok
+    span.finish()
+
+    recordedRequests.head.headers.collectFirst { case h if h.name == "baggage-baggage1" => h.value } shouldBe Some(
+      "hello"
+    )
+  }
+
+  it should "add status code when response is not error" in {
+    val response = basicRequest.post(uri"http://stub/echo").send()
+    response.code shouldBe StatusCode.Ok
+
+    tracer.finishedSpans().asScala.head.tags().asScala(Tags.HTTP_STATUS.getKey) shouldBe StatusCode.Ok.code
+  }
+
+  it should "add logs if response is error" in {
+    Try(basicRequest.post(uri"http://stub/error").send())
+
+    val finishedSpan = tracer
+      .finishedSpans()
+      .asScala
+      .head
+    finishedSpan
+      .logEntries()
+      .asScala
+      .head
+      .fields()
+      .asScala("error.object")
+      .asInstanceOf[RuntimeException]
+      .getMessage shouldBe "something went wrong"
+    finishedSpan.tags().asScala(Tags.ERROR.getKey) shouldBe java.lang.Boolean.TRUE
+  }
+
+  it should "add standard tags during http call" in {
+    val url = uri"http://stub/echo"
+    basicRequest.post(url).send()
+    val tags = tracer.finishedSpans().asScala.head.tags().asScala
+    tags(Tags.HTTP_METHOD.getKey) shouldBe Method.POST.method
+    tags(Tags.HTTP_URL.getKey) shouldBe url.toJavaUri.toString
+    tags(Tags.SPAN_KIND.getKey) shouldBe Tags.SPAN_KIND_CLIENT
+    tags(Tags.COMPONENT.getKey) shouldBe "opentracing-sttp-client"
+  }
+}
