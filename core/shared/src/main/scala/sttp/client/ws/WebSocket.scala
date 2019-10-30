6375db2a89a26d5485c27e30c70b9da64043735f
adamw <adam@warski.org>
2019-10-30 13:34:46 +0100

    Improve websocket receive code

--- core/shared/src/main/scala/sttp/client/ws/WebSocket.scala
+++ core/shared/src/main/scala/sttp/client/ws/WebSocket.scala
@@ -19,10 +19,10 @@ trait WebSocket[F[_]] {
     * Receive a single data frame, ignoring others. The frame might be a fragment.
     * @param pongOnPing Should a [[WebSocketFrame.Pong]] be sent when a [[WebSocketFrame.Ping]] is received.
     */
-  def receiveDataFrame(pongOnPing: Boolean = true): F[Either[WebSocketEvent.Close, WebSocketFrame.Data]] =
+  def receiveDataFrame(pongOnPing: Boolean = true): F[Either[WebSocketEvent.Close, WebSocketFrame.Data[_]]] =
     receive.flatMap {
-      case Left(close)                   => (Left(close): Either[WebSocketEvent.Close, WebSocketFrame.Data]).unit
-      case Right(d: WebSocketFrame.Data) => (Right(d): Either[WebSocketEvent.Close, WebSocketFrame.Data]).unit
+      case Left(close)                      => (Left(close): Either[WebSocketEvent.Close, WebSocketFrame.Data[_]]).unit
+      case Right(d: WebSocketFrame.Data[_]) => (Right(d): Either[WebSocketEvent.Close, WebSocketFrame.Data[_]]).unit
       case Right(WebSocketFrame.Ping(payload)) if pongOnPing =>
         send(WebSocketFrame.Pong(payload)).flatMap(_ => receiveDataFrame(pongOnPing))
       case _ => receiveDataFrame(pongOnPing)
@@ -57,34 +57,30 @@ trait WebSocket[F[_]] {
     * Ignores non-text frames and returns combined results.
     * @param pongOnPing Should a [[WebSocketFrame.Pong]] be sent when a [[WebSocketFrame.Ping]] is received.
     */
-  def receiveText(pongOnPing: Boolean = true): F[Either[WebSocketEvent.Close, String]] = {
-    receiveTextFrame(pongOnPing)
-      .flatMap {
-        case Left(close) => (Left(close): Either[WebSocketEvent.Close, String]).unit
-        case Right(WebSocketFrame.Text(payload, false, _)) =>
-          receiveText(pongOnPing).flatMap {
-            case Left(close) => (Left(close): Either[WebSocketEvent.Close, String]).unit
-            case Right(t)    => (Right(payload + t): Either[WebSocketEvent.Close, String]).unit
-          }
-        case Right(WebSocketFrame.Text(payload, true, _)) => (Right(payload): Either[WebSocketEvent.Close, String]).unit
-      }
-  }
+  def receiveText(pongOnPing: Boolean = true): F[Either[WebSocketEvent.Close, String]] =
+    receiveConcat(receiveTextFrame(pongOnPing), _ + _)
 
   /**
     * Receive a single binary message (which might come from multiple, fragmented frames).
     * Ignores non-binary frames and returns combined results.
     * @param pongOnPing Should a [[WebSocketFrame.Pong]] be sent when a [[WebSocketFrame.Ping]] is received.
     */
-  def receiveBinary(pongOnPing: Boolean): F[Either[WebSocketEvent.Close, Array[Byte]]] = {
-    receiveBinaryFrame(pongOnPing).flatMap {
-      case Left(close) => (Left(close): Either[WebSocketEvent.Close, Array[Byte]]).unit
-      case Right(WebSocketFrame.Binary(payload, false, _)) =>
-        receiveBinary(pongOnPing).flatMap {
-          case Left(close) => (Left(close): Either[WebSocketEvent.Close, Array[Byte]]).unit
-          case Right(t)    => (Right(payload ++ t): Either[WebSocketEvent.Close, Array[Byte]]).unit
+  def receiveBinary(pongOnPing: Boolean): F[Either[WebSocketEvent.Close, Array[Byte]]] =
+    receiveConcat(receiveBinaryFrame(pongOnPing), _ ++ _)
+
+  private def receiveConcat[T, U <: WebSocketFrame.Data[T]](
+      receiveSingle: F[Either[WebSocketEvent.Close, U]],
+      combine: (T, T) => T
+  ): F[Either[WebSocketEvent.Close, T]] = {
+    receiveSingle.flatMap {
+      case Left(close) => (Left(close): Either[WebSocketEvent.Close, T]).unit
+      case Right(data) if !data.finalFragment =>
+        receiveConcat(receiveSingle, combine).flatMap {
+          case Left(close) => (Left(close): Either[WebSocketEvent.Close, T]).unit
+          case Right(t)    => (Right(combine(data.payload, t)): Either[WebSocketEvent.Close, T]).unit
         }
-      case Right(WebSocketFrame.Binary(payload, true, _)) =>
-        (Right(payload): Either[WebSocketEvent.Close, Array[Byte]]).unit
+      case Right(data) if data.finalFragment =>
+        (Right(data.payload): Either[WebSocketEvent.Close, T]).unit
     }
   }
 
--- model/shared/src/main/scala/sttp/model/ws/WebSocketFrame.scala
+++ model/shared/src/main/scala/sttp/model/ws/WebSocketFrame.scala
@@ -4,7 +4,11 @@ sealed trait WebSocketFrame
 
 object WebSocketFrame {
   trait Incoming extends WebSocketFrame
-  trait Data extends Incoming
+  trait Data[T] extends Incoming {
+    def payload: T
+    def finalFragment: Boolean
+    def rsv: Option[Int]
+  }
 
   /**
     * A text frame with fragmentation or extension bits.
@@ -13,7 +17,7 @@ object WebSocketFrame {
     * @param finalFragment flag indicating whether or not this is the final fragment
     * @param rsv optional extension bits
     */
-  case class Text(payload: String, finalFragment: Boolean, rsv: Option[Int]) extends Data
+  case class Text(payload: String, finalFragment: Boolean, rsv: Option[Int]) extends Data[String]
 
   /**
     * A binary frame with fragmentation or extension bits.
@@ -22,7 +26,7 @@ object WebSocketFrame {
     * @param finalFragment flag indicating whether or not this is the last fragment
     * @param rsv optional extension bits
     */
-  case class Binary(payload: Array[Byte], finalFragment: Boolean, rsv: Option[Int]) extends Data
+  case class Binary(payload: Array[Byte], finalFragment: Boolean, rsv: Option[Int]) extends Data[Array[Byte]]
 
   case class Ping(payload: Array[Byte]) extends Incoming
   case class Pong(payload: Array[Byte]) extends Incoming
