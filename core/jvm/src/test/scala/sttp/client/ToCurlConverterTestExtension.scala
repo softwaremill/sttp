330a617989727100d10d7a2e0f4e297a56b7f754
ghostbuster91 <kghost0@gmail.com>
2019-11-18 17:11:03 +0100

    Replace Path.of with new File.toPath

--- core/jvm/src/test/scala/sttp/client/ToCurlConverterTestExtension.scala
+++ core/jvm/src/test/scala/sttp/client/ToCurlConverterTestExtension.scala
@@ -1,6 +1,6 @@
 package sttp.client
 
-import java.nio.file.Path
+import java.io.File
 
 import org.scalatest.{FlatSpec, Matchers, Suite}
 import sttp.client.internal.SttpFile
@@ -8,7 +8,7 @@ import sttp.client.internal.SttpFile
 trait ToCurlConverterTestExtension { suit: Suite with FlatSpec with Matchers =>
   it should "render multipart form data if content is a file" in {
     basicRequest
-      .multipartBody(multipartSttpFile("upload", SttpFile.fromPath(Path.of("myDataSet"))))
+      .multipartBody(multipartSttpFile("upload", SttpFile.fromPath(new File("myDataSet").toPath)))
       .post(uri"http://localhost")
       .toCurl should include(
       """--form 'upload=@myDataSet'"""
