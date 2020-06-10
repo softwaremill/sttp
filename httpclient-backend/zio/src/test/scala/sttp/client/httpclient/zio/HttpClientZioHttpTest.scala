b742779343d2418f7927bf80b95a33b22fb171f3
Lech Głowiak <lech.glowiak@gmail.com>
2020-06-10 10:40:40 +0200

    Fixed byte buffer

--- httpclient-backend/zio/src/test/scala/sttp/client/httpclient/zio/HttpClientZioHttpTest.scala
+++ httpclient-backend/zio/src/test/scala/sttp/client/httpclient/zio/HttpClientZioHttpTest.scala
@@ -5,7 +5,8 @@ import sttp.client.{NothingT, SttpBackend}
 import sttp.client.impl.zio._
 import sttp.client.testing.{ConvertToFuture, HttpTest}
 
-class HttpClientZioHttpTest extends HttpTest[BlockingTask]{
-  override implicit val backend: SttpBackend[BlockingTask, Nothing, NothingT] = runtime.unsafeRun(HttpClientZioBackend())
+class HttpClientZioHttpTest extends HttpTest[BlockingTask] {
+  override implicit val backend: SttpBackend[BlockingTask, Nothing, NothingT] =
+    runtime.unsafeRun(HttpClientZioBackend())
   override implicit val convertToFuture: ConvertToFuture[BlockingTask] = convertZioBlockingTaskToFuture
 }
