49809ad2badaf19d0ca26f93d68b7d43362f9e50
ekalashnikov <eugene.kalashnikov@agoda.com>
2020-07-16 16:36:53 +0700

    Let HttpUrlConnectionBackend allow custom encodings

--- core/src/main/scalajvm/sttp/client/HttpURLConnectionBackend.scala
+++ core/src/main/scalajvm/sttp/client/HttpURLConnectionBackend.scala
@@ -285,8 +285,7 @@ class HttpURLConnectionBackend private (
       case None            => is
       case Some("gzip")    => new GZIPInputStream(is)
       case Some("deflate") => new InflaterInputStream(is)
-      case Some(ce) =>
-        throw new UnsupportedEncodingException(s"Unsupported encoding: $ce")
+      case _               => is
     }
 
   private def adjustExceptions[T](t: => T): T =
