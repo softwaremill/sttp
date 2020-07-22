c4b79eb559c113a006a03955c91e258d1cf827e5
ghostbuster91 <kghost0@gmail.com>
2020-07-22 14:38:56 +0200

    Replace response error with choice

--- json/circe/src/main/scala/sttp/client/circe/SttpCirceApi.scala
+++ json/circe/src/main/scala/sttp/client/circe/SttpCirceApi.scala
@@ -1,5 +1,6 @@
 package sttp.client.circe
 
+import io.circe
 import sttp.client._
 import io.circe.parser.decode
 import io.circe.{Decoder, Encoder, Printer}
@@ -20,9 +21,34 @@ trait SttpCirceApi {
     * - `Left(HttpError(String))` if the response code was other than 2xx (deserialization is not attempted)
     * - `Left(DeserializationError)` if there's an error during deserialization
     */
-  def asJson[B: Decoder: IsOption]: ResponseAs[Either[ResponseError[io.circe.Error], B], Nothing] =
-    asString.mapWithMetadata(ResponseAs.deserializeRightWithError(deserializeJson))
+  def asJson[B: Decoder: IsOption]: ResponseAs[Choice[HttpError[String],DeserializationError[io.circe.Error],B], Nothing] =
+    ???
 
+  def unsafeHttp[L<: Exception,M,R] : Choice[L,M,R] => Choice[Nothing,M,R]= {
+    case Choice.Left(value) => throw value
+    case Choice.Middle(value) => Choice.Middle(value)
+    case Choice.Right(value) => Choice.Right(value)
+  }
+  
+  def unsafeDeserialization[L,M<:Exception,R] : Choice[L,M,R] => Choice[L,Nothing,R]= {
+    case Choice.Left(value) => Choice.Left(value)
+    case Choice.Middle(value) => throw value
+    case Choice.Right(value) => Choice.Right(value)
+  }
+  
+  {
+    val l : Either[Nothing, String ]  = ???
+    l.merge
+    import sttp.client.circe.SttpCirceApi._
+
+    val value: ResponseAs[Choice[HttpError[String], DeserializationError[circe.Error], String], Nothing] = asJson[String]
+    val value1: ResponseAs[Either[DeserializationError[circe.Error], String], Nothing] = value.map(unsafeHttp).map(_.merge)
+    val value2: ResponseAs[Either[HttpError[String], String], Nothing] = asJson[String].map(unsafeDeserialization).map(_.merge)
+    val value3: ResponseAs[String, Nothing] = asJson[String].map(unsafeHttp).map(unsafeDeserialization).map(_.merge)
+  }
+  
+
+  
   /**
     * Tries to deserialize the body from a string into JSON, regardless of the response code. Returns:
     * - `Right(b)` if the parsing was successful
@@ -40,4 +66,44 @@ trait SttpCirceApi {
 
   def deserializeJson[B: Decoder: IsOption]: String => Either[io.circe.Error, B] =
     JsonInput.sanitize[B].andThen(decode[B])
+}
+
+object SttpCirceApi {
+  trait =:!=[A, B]
+
+  implicit def neq[A, B] : A =:!= B = new =:!=[A, B] {}
+  implicit def neqAmbig1[A] : A =:!= A = ???
+  implicit def neqAmbig2[A] : A =:!= A = ???
+  
+  implicit class MergeableChoice[A,B](private val x: Choice[B, B, A])(implicit ev: B =:= A) {
+    def merge: A = x match {
+      case Choice.Right(a) => a
+      case Choice.Middle(a) => ev.apply(a)
+      case Choice.Left(a)  => ev.apply(a)
+    }
+  }
+
+  implicit class PartiallyMergeableChoice2[A,B](private val x: Choice[B, B, A])(implicit ev: B =:!= A) {
+    def merge: Either[B,A] = x match {
+      case Choice.Right(a) => Right(a)
+      case Choice.Middle(a) => Left(a)
+      case Choice.Left(a)  => Left(a)
+    }
+  }
+}
+
+sealed abstract class Choice[+L,+M,+R]() {
+  def triMap[L2,M2,R2](f: L=>L2)(g: M=>M2)(h: R=>R2) : Choice[L2,M2,R2] = {
+    this match {
+      case Choice.Left(value) => Choice.Left(f(value))
+      case Choice.Middle(value) =>Choice.Middle(g(value))
+      case Choice.Right(value) =>Choice.Right(h(value))
+    }
+  }
+}
+
+object Choice {
+  case class Left[L](value: L) extends Choice[L, Nothing, Nothing]
+  case class Middle[M](value: M) extends Choice[Nothing, M, Nothing]
+  case class Right[R](value: R) extends Choice[Nothing,Nothing, R]
 }
