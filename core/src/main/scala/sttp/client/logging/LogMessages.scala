cdbe3b65dcd9b71233078624273be1abfc877064
adamw <adam@warski.org>
2020-08-25 16:07:29 +0200

    #190: Improve the logging backend

--- core/src/main/scala/sttp/client/logging/LogMessages.scala
+++ core/src/main/scala/sttp/client/logging/LogMessages.scala
@@ -1,6 +1,17 @@
 package sttp.client.logging
 
-import sttp.client.{Request, Response}
+import sttp.client.{
+  ByteArrayBody,
+  ByteBufferBody,
+  FileBody,
+  InputStreamBody,
+  MultipartBody,
+  NoBody,
+  Request,
+  Response,
+  StreamBody,
+  StringBody
+}
 import sttp.model.Header
 
 /**
@@ -8,17 +19,22 @@ import sttp.model.Header
   */
 object LogMessages {
   def beforeRequestSend(request: Request[_, _]): String =
-    s"Sending request: ${requestToString(request)}"
+    s"Sending request: ${requestToString(request, withBody = false)}"
 
-  def response(request: Request[_, _], response: Response[_]): String =
-    s"For request: ${requestToString(request)}, got response: ${responseToString(response)}"
+  def response(
+      request: Request[_, _],
+      response: Response[_],
+      withRequestBody: Boolean,
+      responseBody: Option[String]
+  ): String =
+    s"For request: ${requestToString(request, withRequestBody)}, got response: ${responseToString(response, responseBody)}"
 
-  def requestException(request: Request[_, _]): String =
-    s"Exception when sending request: ${requestToString(request)}"
+  def requestException(request: Request[_, _], withBody: Boolean): String =
+    s"Exception when sending request: ${requestToString(request, withBody)}"
 
   def requestTiming(request: Request[_, _], result: String, elapsed: Long): String = {
     val elapsedStr = f"${elapsed / 1000.0}%.3fs"
-    s"For request: ${requestToString(request)}, got response: $result, took: $elapsedStr"
+    s"For request: ${requestToString(request, withBody = false)}, got response: $result, took: $elapsedStr"
   }
 
   def requestCurl(request: Request[_, _], result: String): String =
@@ -26,13 +42,28 @@ object LogMessages {
 
   //
 
-  def requestToString(request: Request[_, _]): String = {
+  def requestToString(request: Request[_, _], withBody: Boolean): String = {
     val ws = if (request.isWebSocket) " (WebSocket) " else ""
-    s"${request.method}$ws ${request.uri}"
+    val body = if (withBody) " " + requestBodyToString(request) else ""
+    s"${request.method}$ws ${request.uri}$body"
   }
 
-  def responseToString(response: Response[_]): String =
-    s"${response.code}, headers: ${headersToString(response.headers)}"
+  def responseToString(response: Response[_], responseBody: Option[String]): String =
+    s"${response.code}, headers: ${headersToString(response.headers)}${responseBody.map(", body: " + _).getOrElse("")}"
 
   def headersToString(headers: Seq[Header]): String = headers.map(_.toStringSafe).mkString(", ")
+
+  //
+
+  def requestBodyToString(request: Request[_, _]): String =
+    request.body match {
+      case NoBody                => "(empty body)"
+      case StringBody(s, _, _)   => s
+      case ByteArrayBody(_, _)   => "(byte array)"
+      case ByteBufferBody(_, _)  => "(byte buffer)"
+      case InputStreamBody(_, _) => "(input stream)"
+      case FileBody(f, _)        => s"(file ${f.name})"
+      case StreamBody(_)         => "(stream)"
+      case MultipartBody(parts)  => s"(multipart: ${parts.map(p => p.name).mkString(",")})"
+    }
 }
--- logging/slf4j/src/main/scala/sttp/client/logging/slf4j/Slf4jLoggingBackend.scala
+++ logging/slf4j/src/main/scala/sttp/client/logging/slf4j/Slf4jLoggingBackend.scala
@@ -1,31 +1,56 @@
 package sttp.client.logging.slf4j
 
 import org.slf4j.{Logger, LoggerFactory}
+import sttp.capabilities.Effect
 import sttp.client._
 import sttp.client.listener.{ListenerBackend, RequestListener}
 import sttp.client.logging.LogMessages
+import sttp.monad.MonadError
+import sttp.monad.syntax._
 
 object Slf4jLoggingBackend {
   private val logger = LoggerFactory.getLogger("sttp.client.logging.slf4j.Slf4jLoggingBackend")
 
-  def apply[F[_], S](delegate: SttpBackend[F, S]): SttpBackend[F, S] =
-    ListenerBackend.lift(delegate, new Slf4jLoggingListener(logger))
+  def apply[F[_], S](
+      delegate: SttpBackend[F, S],
+      logRequestBody: Boolean = false,
+      logResponseBody: Boolean = false
+  ): SttpBackend[F, S] =
+    if (logResponseBody) new Slf4jLoggingWithResponseBodyBackend(delegate, logger, logRequestBody)
+    else
+      ListenerBackend.lift(delegate, new Slf4jLoggingListener(logger, logRequestBody))
 }
 
-class Slf4jLoggingListener(logger: Logger) extends RequestListener[Identity, Unit] {
+class Slf4jLoggingListener(logger: Logger, logRequestBody: Boolean) extends RequestListener[Identity, Unit] {
   override def beforeRequest(request: Request[_, _]): Identity[Unit] = {
     logger.debug(LogMessages.beforeRequestSend(request))
   }
 
   override def requestException(request: Request[_, _], tag: Unit, e: Exception): Identity[Unit] = {
-    logger.error(LogMessages.requestException(request), e)
+    logger.error(LogMessages.requestException(request, logRequestBody), e)
   }
 
   override def requestSuccessful(request: Request[_, _], response: Response[_], tag: Unit): Identity[Unit] = {
-    if (response.isSuccess) {
-      logger.debug(LogMessages.response(request, response))
-    } else {
-      logger.debug(LogMessages.response(request, response))
-    }
+    logger.debug(LogMessages.response(request, response, logRequestBody, None))
   }
+}
+
+class Slf4jLoggingWithResponseBodyBackend[F[_], S](delegate: SttpBackend[F, S], logger: Logger, logRequestBody: Boolean)
+    extends SttpBackend[F, S] {
+  override def send[T, R >: S with Effect[F]](request: Request[T, R]): F[Response[T]] = {
+    (for {
+      _ <- responseMonad.eval(logger.debug(LogMessages.beforeRequestSend(request)))
+      response <- request.response(asBothOption(request.response, asStringAlways)).send(delegate)
+      _ <- responseMonad.eval(logger.debug(LogMessages.response(request, response, logRequestBody, response.body._2)))
+    } yield response.copy(body = response.body._1))
+      .handleError {
+        case e: Exception =>
+          responseMonad
+            .eval(logger.error(LogMessages.requestException(request, logRequestBody), e))
+            .flatMap(_ => responseMonad.error(e))
+      }
+  }
+
+  override def close(): F[Unit] = delegate.close()
+  override implicit def responseMonad: MonadError[F] = delegate.responseMonad
 }
