8b97a0762f51b079d3f7285b95e93f60e9ab6f75
Adam Smolarek <adam.smolarek@softwaremill.pl>
2020-12-04 10:26:11 +0100

    validation

--- implementations/fs2/src/main/scala/sttp/client3/impl/fs2/FS2ServerSentEvents.scala
+++ implementations/fs2/src/main/scala/sttp/client3/impl/fs2/FS2ServerSentEvents.scala
@@ -3,6 +3,8 @@ package sttp.client3.impl.fs2
 import fs2.text
 import sttp.client3.sse.ServerSentEvent
 
+import scala.util.Try
+
 object FS2ServerSentEvents {
   def decodeSSE[F[_]](response: fs2.Stream[F, Byte]): fs2.Stream[F, ServerSentEvent] = {
     response
@@ -15,9 +17,9 @@ object FS2ServerSentEvents {
           line.span(_ == ':') match {
             case ("data", content)      => combineData(event, content)
             case ("id", content)        => event.copy(id = Some(content))
-            //todo add number validation it should be positive integer
-            case ("retry", content)     => event.copy(retry = Some(content.toInt))
+            case ("retry", content)     => event.copy(retry = Try(content.toInt).toOption)
             case ("eventType", content) => event.copy(eventType = Some(content))
+            case _                      => event
           }
         }
       }
