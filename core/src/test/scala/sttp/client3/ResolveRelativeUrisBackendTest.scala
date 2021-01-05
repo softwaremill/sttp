70b5deeaa4af67e9f540dc44b4d5d357b49f69a2
adamw <adam@warski.org>
2021-01-05 17:40:39 +0100

    Make test JS-compatible

--- core/src/test/scala/sttp/client3/ResolveRelativeUrisBackendTest.scala
+++ core/src/test/scala/sttp/client3/ResolveRelativeUrisBackendTest.scala
@@ -2,32 +2,35 @@ package sttp.client3
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import sttp.client3.testing.{RecordingSttpBackend, SttpBackendStub}
+import sttp.client3.testing.SttpBackendStub
+import sttp.model.StatusCode
 
 class ResolveRelativeUrisBackendTest extends AnyFlatSpec with Matchers {
   it should "not resolve absolute URIs" in {
     // given
-    val delegate = new RecordingSttpBackend(SttpBackendStub.synchronous.whenAnyRequest.thenRespondOk())
+    val delegate = SttpBackendStub.synchronous.whenRequestMatchesPartial { case r =>
+      Response(r.uri.toString, StatusCode.Ok)
+    }
     val backend = ResolveRelativeUrisBackend(delegate, uri"http://example.org")
 
     // when
-    basicRequest.get(uri"http://example2.org/test?a=1").send(backend)
+    val response = basicRequest.response(asStringAlways).get(uri"http://example2.org/test?a=1").send(backend)
 
     // then
-    delegate.allInteractions should have size (1)
-    delegate.allInteractions.head._1.uri.toString shouldBe "http://example2.org/test?a=1"
+    response.body shouldBe "http://example2.org/test?a=1"
   }
 
   it should "resolve relative URIs" in {
     // given
-    val delegate = new RecordingSttpBackend(SttpBackendStub.synchronous.whenAnyRequest.thenRespondOk())
+    val delegate = SttpBackendStub.synchronous.whenRequestMatchesPartial { case r =>
+      Response(r.uri.toString, StatusCode.Ok)
+    }
     val backend = ResolveRelativeUrisBackend(delegate, uri"http://example.org")
 
     // when
-    basicRequest.get(uri"/test?a=1").send(backend)
+    val response = basicRequest.response(asStringAlways).get(uri"/test?a=1").send(backend)
 
     // then
-    delegate.allInteractions should have size (1)
-    delegate.allInteractions.head._1.uri.toString shouldBe "http://example.org/test?a=1"
+    response.body shouldBe "http://example.org/test?a=1"
   }
 }
