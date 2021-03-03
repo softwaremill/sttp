40990b78d252a0691d108eeb739b6c59a8ddb451
Yann Simon <yann.simon@commercetools.de>
2021-03-03 15:43:02 +0100

    migrate armeria to CE3

--- armeria-backend/fs2/src/main/scala/sttp/client3/armeria/fs2/ArmeriaFs2Backend.scala
+++ armeria-backend/fs2/src/main/scala/sttp/client3/armeria/fs2/ArmeriaFs2Backend.scala
@@ -1,6 +1,7 @@
 package sttp.client3.armeria.fs2
 
-import cats.effect.{ConcurrentEffect, Resource, Sync}
+import cats.effect.std.Dispatcher
+import cats.effect.{Async, Resource, Sync}
 import com.linecorp.armeria.client.WebClient
 import com.linecorp.armeria.common.HttpData
 import com.linecorp.armeria.common.stream.StreamMessage
@@ -14,7 +15,7 @@ import sttp.client3.impl.cats.CatsMonadAsyncError
 import sttp.client3.{FollowRedirectsBackend, SttpBackend, SttpBackendOptions}
 import sttp.monad.MonadAsyncError
 
-private final class ArmeriaFs2Backend[F[_]: ConcurrentEffect](client: WebClient, closeFactory: Boolean)
+private final class ArmeriaFs2Backend[F[_]: Async](client: WebClient, closeFactory: Boolean)(implicit dispatcher: Dispatcher[F])
     extends AbstractArmeriaBackend[F, Fs2Streams[F]](client, closeFactory, new CatsMonadAsyncError) {
 
   override val streams: Fs2Streams[F] = Fs2Streams[F]
@@ -27,16 +28,15 @@ private final class ArmeriaFs2Backend[F[_]: ConcurrentEffect](client: WebClient,
       override implicit val monad: MonadAsyncError[F] = new CatsMonadAsyncError
 
       override def publisherToStream(streamMessage: StreamMessage[HttpData]): Stream[F, Byte] =
-        streamMessage.toStream[F].flatMap(httpData => Stream.chunk(Chunk.bytes(httpData.array())))
+        streamMessage.toStream[F].flatMap(httpData => Stream.chunk(Chunk.array(httpData.array())))
     }
 
   override protected def streamToPublisher(stream: Stream[F, Byte]): Publisher[HttpData] =
-    stream.chunks
-      .map(chunk => {
-        val bytes = chunk.toBytes
+    StreamUnicastPublisher(stream.chunks
+      .map { chunk =>
+        val bytes = chunk.compact
         HttpData.wrap(bytes.values, bytes.offset, bytes.length)
-      })
-      .toUnicastPublisher
+      }, dispatcher)
 }
 
 object ArmeriaFs2Backend {
@@ -45,24 +45,24 @@ object ArmeriaFs2Backend {
     * the client will manage its own connection pool. If you'd like to reuse the default Armeria `ClientFactory`,
     * use `.usingDefaultClient`.
     */
-  def apply[F[_]: ConcurrentEffect](
+  def apply[F[_]: Async: Dispatcher](
       options: SttpBackendOptions = SttpBackendOptions.Default
   ): SttpBackend[F, Fs2Streams[F]] =
     apply(newClient(options), closeFactory = true)
 
-  def resource[F[_]: ConcurrentEffect](
+  def resource[F[_]: Async](
       options: SttpBackendOptions = SttpBackendOptions.Default
-  ): Resource[F, SttpBackend[F, Fs2Streams[F]]] = {
-    Resource.make(Sync[F].delay(apply(newClient(options), closeFactory = true)))(_.close())
-  }
+  ): Resource[F, SttpBackend[F, Fs2Streams[F]]] =
+    Dispatcher[F].flatMap(implicit dispatcher =>
+      Resource.make(Sync[F].delay(apply(newClient(options), closeFactory = true)))(_.close()))
 
-  def usingClient[F[_]: ConcurrentEffect](client: WebClient): SttpBackend[F, Fs2Streams[F]] =
+  def usingClient[F[_]: Async: Dispatcher](client: WebClient): SttpBackend[F, Fs2Streams[F]] =
     apply(client, closeFactory = false)
 
-  def usingDefaultClient[F[_]: ConcurrentEffect](): SttpBackend[F, Fs2Streams[F]] =
+  def usingDefaultClient[F[_]: Async: Dispatcher](): SttpBackend[F, Fs2Streams[F]] =
     apply(newClient(), closeFactory = false)
 
-  private def apply[F[_]: ConcurrentEffect](
+  private def apply[F[_]: Async: Dispatcher](
       client: WebClient,
       closeFactory: Boolean
   ): SttpBackend[F, Fs2Streams[F]] =
