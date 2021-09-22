a3228c7dc0d284f1554deeabdac12e4dc8a0b668
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-09-22 12:05:36 +0200

    Changed condition for body decoding in Http4sBackend

--- http4s-ce2-backend/src/main/scala/sttp/client3/http4s/Http4sBackend.scala
+++ http4s-ce2-backend/src/main/scala/sttp/client3/http4s/Http4sBackend.scala
@@ -8,7 +8,7 @@ import cats.effect.{Blocker, Concurrent, ConcurrentEffect, ContextShift, Resourc
 import cats.implicits._
 import cats.effect.implicits._
 import fs2.{Chunk, Stream}
-import org.http4s.{ContentCoding, EntityBody, Request => Http4sRequest}
+import org.http4s.{ContentCoding, EntityBody, Status, Request => Http4sRequest}
 import org.http4s
 import org.http4s.client.Client
 import org.http4s.blaze.client.BlazeClientBuilder
@@ -167,7 +167,7 @@ class Http4sBackend[F[_]: ConcurrentEffect: ContextShift](
   private def decompressResponseBody(hr: http4s.Response[F]): http4s.Response[F] = {
     val body = hr.headers
       .get[`Content-Encoding`]
-      .filter(e => !(!hr.isChunked && e.contentCoding.equals(ContentCoding.gzip)))
+      .filterNot(_ => hr.status.equals(Status.NoContent))
       .map(e => customEncodingHandler.orElse(EncodingHandler(standardEncodingHandler))(hr.body -> e.contentCoding))
       .getOrElse(hr.body)
     hr.copy(body = body)
