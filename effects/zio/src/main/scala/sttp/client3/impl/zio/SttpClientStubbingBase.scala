31cfc4a96a89336b05ff16a54630b76fca04a708
adamw <adam@warski.org>
2022-01-18 15:33:12 +0100

    Fix SttpClientStubbing for async-http-client backend

--- effects/zio/src/main/scala/sttp/client3/impl/zio/SttpClientStubbingBase.scala
+++ effects/zio/src/main/scala/sttp/client3/impl/zio/SttpClientStubbingBase.scala
@@ -64,7 +64,7 @@ trait SttpClientStubbingBase[R, P] {
     private def whenRequest(
         f: SttpBackendStub[RIO[R, *], P]#WhenRequest => SttpBackendStub[RIO[R, *], P]
     ): URIO[SttpClientStubbing, Unit] =
-      URIO.serviceWith(_.update(stub => f(stub.whenRequestMatches(p))))
+      URIO.serviceWithZIO((_: SttpClientStubbing).update(stub => f(stub.whenRequestMatches(p))))
   }
 
   val layer: ZLayer[Any, Nothing, SttpClientStubbing with SttpBackend[RIO[R, *], P]] = {
