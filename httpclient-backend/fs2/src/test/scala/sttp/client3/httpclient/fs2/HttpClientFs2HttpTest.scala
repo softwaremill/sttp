e84751f68f96f084bea50747d6f925a798f840f6
Pask <pask423th@gmail.com>
2022-04-18 22:27:28 +0200

    Removed old httpclient classes

--- httpclient-backend/fs2/src/test/scala/sttp/client3/httpclient/fs2/HttpClientFs2HttpTest.scala
+++
@@ -1,8 +0,0 @@
-package sttp.client3.httpclient.fs2
-
-import cats.effect.IO
-import sttp.client3.testing.HttpTest
-
-class HttpClientFs2HttpTest extends HttpTest[IO] with HttpClientFs2TestBase {
-  override def supportsHostHeaderOverride = false
-}
