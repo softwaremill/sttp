b478576ad559e9ee7b99ad8f7eb465743c278170
Mark de Jong <Fristi@users.noreply.github.com>
2022-08-19 09:18:23 +0200

    Formatting

--- effects/zio/src/main/scalajvm/sttp/client3/httpclient/zio/package.scala
+++ effects/zio/src/main/scalajvm/sttp/client3/httpclient/zio/package.scala
@@ -4,7 +4,6 @@ import sttp.capabilities.WebSockets
 import sttp.capabilities.zio.ZioStreams
 import sttp.client3.SttpBackend
 
-
 package object zio {
   type SttpClient = SttpBackend[_root_.zio.Task, ZioStreams with WebSockets]
 }
