15afd775e4d627e28cac60463fbe8eba535008ac
adamw <adam@warski.org>
2022-08-26 14:15:29 +0200

    Remove the zio SttpClient type alias

--- effects/zio/src/main/scalajvm/sttp/client3/httpclient/zio/package.scala
+++
@@ -1,9 +0,0 @@
-package sttp.client3.httpclient
-
-import sttp.capabilities.WebSockets
-import sttp.capabilities.zio.ZioStreams
-import sttp.client3.SttpBackend
-
-package object zio {
-  type SttpClient = SttpBackend[_root_.zio.Task, ZioStreams with WebSockets]
-}
