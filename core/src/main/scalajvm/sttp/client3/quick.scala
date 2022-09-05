1ff71170b2e1f6d1110603d222dea22fd619828d
adamw <adam@warski.org>
2022-09-05 18:28:19 +0200

    Rename client in quick

--- core/src/main/scalajvm/sttp/client3/quick.scala
+++ core/src/main/scalajvm/sttp/client3/quick.scala
@@ -2,5 +2,5 @@ package sttp.client3
 
 object quick extends SttpApi {
   lazy val backend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
-  lazy val sttpClient: SimpleHttpClient = SimpleHttpClient(backend)
+  lazy val simpleHttpClient: SimpleHttpClient = SimpleHttpClient(backend)
 }
