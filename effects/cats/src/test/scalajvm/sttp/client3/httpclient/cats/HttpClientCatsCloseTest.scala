ac69627123f5fcf127b707902a57ea0c312c0a0d
adamw <adam@warski.org>
2023-01-04 14:51:51 +0100

    Add test

---
+++ effects/cats/src/test/scalajvm/sttp/client3/httpclient/cats/HttpClientCatsCloseTest.scala
@@ -0,0 +1,18 @@
+package sttp.client3.httpclient.cats
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class HttpClientCatsCloseTest extends AsyncFreeSpec with Matchers {
+  "cats-effect" - {
+    "continue working after the backend is closed" in {
+      HttpClientCatsBackend
+        .resource[IO]()
+        .use(_ => IO.unit)
+        .flatMap(_ => IO(succeed).start.flatMap(_.joinWith(IO(fail)))) // the cats executor should still be open
+        .unsafeToFuture()
+    }
+  }
+}
