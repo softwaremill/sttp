7149098d21f91cdff752886444ecc36cf5756419
adamw <adam@warski.org>
2023-02-23 11:29:13 +0100

    Code organization

--- core/src/main/scala/sttp/client3/RequestBuilder.scala
+++
@@ -1,8 +0,0 @@
-package sttp.client3
-
-/** The builder methods of a request. The uri and method are specified.
-  *
-  * @tparam R
-  *   The type of request
-  */
-trait RequestBuilder[+R <: RequestBuilder[R]] extends PartialRequestBuilder[R, R] { self: R => }
