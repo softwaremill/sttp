4304ef05982b68140c72daba85c115689908e539
adamw <adam@warski.org>
2023-03-13 13:46:25 +0100

    Add a parameterless send() extension method for quick requests

--- core/src/main/scalajs/sttp/client4/quick.scala
+++ core/src/main/scalajs/sttp/client4/quick.scala
@@ -4,4 +4,8 @@ import scala.concurrent.Future
 
 object quick extends SttpApi {
   lazy val backend: Backend[Future] = FetchBackend()
+
+  implicit class RichRequest[T](val request: Request[T]) {
+    def send(): Future[Response[T]] = backend.send(request)
+  }
 }
