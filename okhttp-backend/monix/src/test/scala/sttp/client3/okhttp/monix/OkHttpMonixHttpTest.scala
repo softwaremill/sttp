bea95e8a8a28cf763cbdc2fbc62bf14db58a407d
Adam Fish Rybicki <rybickiadam@yahoo.com>
2023-06-21 10:23:36 +0200

    monix test support off

--- okhttp-backend/monix/src/test/scala/sttp/client3/okhttp/monix/OkHttpMonixHttpTest.scala
+++ okhttp-backend/monix/src/test/scala/sttp/client3/okhttp/monix/OkHttpMonixHttpTest.scala
@@ -17,6 +17,7 @@ class OkHttpMonixHttpTest extends HttpTest[Task] {
   override implicit val convertToFuture: ConvertToFuture[Task] = convertMonixTaskToFuture
 
   override def supportsDeflateWrapperChecking = false
+  override def supportsEmptyContentEncoding = false
 
   override def timeoutToNone[T](t: Task[T], timeoutMillis: Int): Task[Option[T]] =
     t.map(Some(_))
