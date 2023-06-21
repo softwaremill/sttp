bec18c0d76d43488122409723ce1b3a0e1e7d475
Adam Fish Rybicki <rybickiadam@yahoo.com>
2023-06-21 07:46:47 +0200

    armeria test update

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -3,15 +3,17 @@ import sttp.client3._
 import sttp.model.{HeaderNames, MediaType}
 
 object Main extends App{
-  //  val backend = HttpClientSyncBackend()
+    val backend = HttpClientSyncBackend()
   //  val backendTwo = HttpURLConnectionBackend(customEncodingHandler = {case (is, "") => is})
   //
   //
-  //  val request: Request[Either[String, String], Any] = basicRequest
-  //    .header(HeaderNames.Accept, MediaType.ApplicationOctetStream.toString())
-  //    .get(uri"https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json")
+    val request: Request[Either[String, String], Any] = basicRequest
+      .header(HeaderNames.Accept, MediaType.ApplicationOctetStream.toString())
+      .get(uri"https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json")
+
+    println(s"reqeust = $request")
   //
-  //  println(s"headers = ${request.headers}")
-  //  val response = request.send(backend)
-  //  println(response)
+//    println(s"headers = ${request.headers}")
+//    val response = request.send(backend)
+//    println(response.headers)
 }
