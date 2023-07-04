30c0452cfdef1765760a0c27a4101a120303618b
Adrian Wydra <50871708+wydra98@users.noreply.github.com>
2023-07-04 08:54:17 +0200

    Header tests in ZIO to proper classes (#1877)

--- armeria-backend/zio1/src/test/scala/sttp/client4/armeria/zio/ArmeriaZioHttpTest.scala
+++ armeria-backend/zio1/src/test/scala/sttp/client4/armeria/zio/ArmeriaZioHttpTest.scala
@@ -3,7 +3,7 @@ package sttp.client4.armeria.zio
 import sttp.client4._
 import sttp.client4.impl.zio.ZioTestBase
 import sttp.client4.testing.{ConvertToFuture, HttpTest}
-import zio.Task
+import zio.{Task, ZIO}
 
 class ArmeriaZioHttpTest extends HttpTest[Task] with ZioTestBase {
 
@@ -14,4 +14,20 @@ class ArmeriaZioHttpTest extends HttpTest[Task] with ZioTestBase {
   override def supportsCancellation = false
   override def supportsAutoDecompressionDisabling = false
   override def supportsDeflateWrapperChecking = false // armeria hangs
+
+  "throw an exception instead of ZIO defect if the header value is invalid" in {
+
+    val r = basicRequest
+      .get(uri"https://example.com")
+      .header("X-Api-Key", " Я ЛЮБЛЮ БОРЩ")
+      .response(asString)
+      .send(backend)
+
+    val resultTask: Task[Any] = r.catchSomeCause {
+      case c if c.defects.nonEmpty => ZIO.fail(new Exception("Defect occurred during the operation"))
+      case _                       => ZIO.succeed("No defects occurred during the operation")
+    }
+
+    convertToFuture.toFuture(resultTask).map(_ => succeed)
+  }
 }
