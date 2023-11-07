b554596f3a7e7dfaea9a7a37c0a71c5786b3d154
Daniil Filippov <flipp5b@gmail.com>
2023-11-07 13:30:35 +0300

    Override blocking inside CatsMonadError
    Fix #1991

---
+++ effects/cats/src/test/scalajvm/sttp/client4/impl/cats/CatsMonadErrorTest.scala
@@ -0,0 +1,30 @@
+package sttp.client4.impl.cats
+
+import cats.effect.IO
+import cats.effect.unsafe.{IORuntime, IORuntimeConfig}
+import org.scalatest.freespec.AsyncFreeSpec
+
+class CatsMonadErrorTest extends AsyncFreeSpec {
+  "blocking" - {
+    "should shift to blocking execution context" in {
+      implicit val ioRuntime: IORuntime = createIORuntime(computePoolSize = 1)
+      val monad = new CatsMonadError[IO]
+
+      val program = monad
+        .blocking(Thread.sleep(100))
+        .background
+        .use(getOutcome => IO.race(getOutcome, IO.unit))
+
+      program
+        .flatMap(either => IO.delay(assert(either.isRight)))
+        .unsafeToFuture()
+    }
+  }
+
+  private def createIORuntime(computePoolSize: Int): IORuntime = {
+    val (compute, _) = IORuntime.createWorkStealingComputeThreadPool(computePoolSize)
+    val (blocking, _) = IORuntime.createDefaultBlockingExecutionContext()
+
+    IORuntime(compute, blocking, compute, () => (), IORuntimeConfig())
+  }
+}
