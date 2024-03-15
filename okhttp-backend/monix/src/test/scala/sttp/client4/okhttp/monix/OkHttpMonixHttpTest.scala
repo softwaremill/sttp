6e25a316162d7c8a0caa5c9c70fcbd32e978b9d5
Roman Janusz <r.janusz.rgz@gmail.com>
2024-03-15 18:18:12 +0100

    using OkHttpHttpTest base class in OkHttpMonixHttpTest

--- okhttp-backend/monix/src/test/scala/sttp/client4/okhttp/monix/OkHttpMonixHttpTest.scala
+++ okhttp-backend/monix/src/test/scala/sttp/client4/okhttp/monix/OkHttpMonixHttpTest.scala
@@ -1,24 +1,22 @@
 package sttp.client4.okhttp.monix
 
-import java.util.concurrent.TimeoutException
-
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import sttp.capabilities.monix.MonixStreams
 import sttp.client4.StreamBackend
 import sttp.client4.impl.monix.convertMonixTaskToFuture
-import sttp.client4.testing.{ConvertToFuture, HttpTest}
+import sttp.client4.okhttp.OkHttpHttpTest
+import sttp.client4.testing.ConvertToFuture
 
-import scala.concurrent.duration.DurationInt
+import java.util.concurrent.TimeoutException
+import scala.concurrent.duration._
 
-class OkHttpMonixHttpTest extends HttpTest[Task] {
+class OkHttpMonixHttpTest extends OkHttpHttpTest[Task] {
 
   override val backend: StreamBackend[Task, MonixStreams] = OkHttpMonixBackend().runSyncUnsafe()
   override implicit val convertToFuture: ConvertToFuture[Task] = convertMonixTaskToFuture
 
   override def supportsDeflateWrapperChecking = false
-
-  override def supportsNonAsciiHeaderValues = false
 
   override def timeoutToNone[T](t: Task[T], timeoutMillis: Int): Task[Option[T]] =
     t.map(Some(_))
