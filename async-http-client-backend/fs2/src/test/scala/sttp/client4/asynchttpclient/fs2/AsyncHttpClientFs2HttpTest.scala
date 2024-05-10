5c0b9e26c0e5c4dcbd2a821477840b51b4f0e7e3
bartlomiej.zylinski <bartlomiej.zylinski@softwaremill.pl>
2024-05-10 12:01:14 +0200

    Remove supportsConnectionRefusedTest flag

--- async-http-client-backend/fs2/src/test/scala/sttp/client4/asynchttpclient/fs2/AsyncHttpClientFs2HttpTest.scala
+++ async-http-client-backend/fs2/src/test/scala/sttp/client4/asynchttpclient/fs2/AsyncHttpClientFs2HttpTest.scala
@@ -8,7 +8,4 @@ import sttp.client4.impl.cats.{CatsTestBase, TestIODispatcher}
 class AsyncHttpClientFs2HttpTest extends AsyncHttpClientHttpTest[IO] with TestIODispatcher with CatsTestBase {
   override val backend: Backend[IO] =
     AsyncHttpClientFs2Backend[IO](dispatcher = dispatcher).unsafeRunSync()
-
-  // for some unknown reason this single test fails using the fs2 implementation
-  override def supportsConnectionRefusedTest = true
 }
