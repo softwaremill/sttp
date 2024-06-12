490779539b29047a187a641c1f5d586486d4a629
kciesielski <kciesielski@users.noreply.github.com>
2024-06-12 11:37:24 +0200

    SSE support based on Ox

---
+++ effects/ox/src/test/scala/sttp/client4/impl/ox/ws/OxSseTest.scala
@@ -0,0 +1,35 @@
+package sttp.client4.impl.ox.sse
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import ox.*
+import ox.IO.globalForTesting.given
+import sttp.client4.*
+import sttp.client4.testing.HttpTest.*
+import sttp.model.sse.ServerSentEvent
+
+class OxServerSentEventsTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll:
+
+  lazy val backend: WebSocketSyncBackend = DefaultSyncBackend()
+
+  behavior of "OxServerSentEvents"
+
+  it should "parse SSEs" in supervised {
+    val sseData = "text1 in line1\ntext2 in line2"
+    val expectedEvent = ServerSentEvent(data = Some(sseData), eventType = Some("test-event"), retry = Some(42000))
+    val expectedEvents =
+      Seq(expectedEvent.copy(id = Some("1")), expectedEvent.copy(id = Some("2")), expectedEvent.copy(id = Some("3")))
+    basicRequest
+      .post(uri"$endpoint/sse/echo3")
+      .body(sseData)
+      .response(asInputStreamAlways { is =>
+        OxServerSentEvents.parse(is).take(3).toList shouldBe(expectedEvents) 
+        ()
+      })
+      .send(backend)
+  }
+  
+  override protected def afterAll(): Unit =
+    backend.close()
+    super.afterAll()
