a043d98a400e818420abd41b736c1402eae99f02
adamw <adam@warski.org>
2024-07-19 19:21:59 +0200

    RTD migration

--- generated-docs/out/conf.py
+++ generated-docs/out/conf.py
@@ -20,6 +20,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# https://about.readthedocs.com/blog/2024/07/addons-by-default/
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
 
 # -- General configuration ------------------------------------------------
 
