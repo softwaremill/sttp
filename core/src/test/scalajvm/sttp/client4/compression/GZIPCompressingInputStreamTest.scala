e4172a49d2088678428fb80220e96cdc6870885d
adamw <adam@warski.org>
2024-12-30 22:21:42 +0100

    Test

--- core/src/test/scalajvm/sttp/client4/compression/GZIPCompressingInputStreamTest.scala
+++ core/src/test/scalajvm/sttp/client4/compression/GZIPCompressingInputStreamTest.scala
@@ -6,6 +6,9 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 import java.io.ByteArrayInputStream
 import java.util.zip.GZIPInputStream
+import java.nio.file.Files
+import java.io.File
+import java.io.FileInputStream
 
 class GZIPCompressingInputStreamTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
   implicit override val generatorDrivenConfig =
@@ -17,5 +20,22 @@ class GZIPCompressingInputStreamTest extends AnyFlatSpec with Matchers with Scal
       val decompressed = new GZIPInputStream(compressedStream).readAllBytes()
       decompressed shouldEqual input
     }
+  }
+
+  it should "compress data from a file" in {
+    val testFileContent = "test file content"
+    withTemporaryFile(testFileContent.getBytes()) { file =>
+      val gzipInputStream = new GZIPInputStream(new GZIPCompressingInputStream(new FileInputStream(file)))
+      val decompressedBytes = gzipInputStream.readAllBytes()
+      decompressedBytes shouldEqual testFileContent.getBytes()
+    }
+  }
+
+  private def withTemporaryFile[T](content: Array[Byte])(f: File => T): T = {
+    val file = Files.createTempFile("sttp", "sttp")
+    Files.write(file, content)
+
+    try f(file.toFile)
+    finally { val _ = Files.deleteIfExists(file) }
   }
 }
