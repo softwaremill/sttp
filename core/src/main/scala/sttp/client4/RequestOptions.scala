46cc1313b55dc72695b3ff1548bd1744310f9332
adamw <adam@warski.org>
2025-01-16 09:58:08 +0100

    Typo

--- core/src/main/scala/sttp/client4/RequestOptions.scala
+++ core/src/main/scala/sttp/client4/RequestOptions.scala
@@ -10,12 +10,12 @@ import sttp.client4.logging.LoggingOptions
   *   Should the response body be decompressed, if a `Content-Encoding` header is present. By default, backends support
   *   [[sttp.model.Encodings.Gzip]] and [[sttp.model.Encodings.Deflate]] encodings, but others might available as well;
   *   refer to the backend documentation for details. If an encoding is not supported, an exception is thrown / a failed
-  *   effet returned, when sending the request.
+  *   effect returned, when sending the request.
   * @param compressRequestBody
   *   Should the request body be compressed, and if so, with which encoding. By default, backends support
   *   [[sttp.model.Encodings.Gzip]] and [[sttp.model.Encodings.Deflate]] encodings, but others might available as well;
   *   refer to the backend documentation for details. If an encoding is not supported, an exception is thrown / a failed
-  *   effet returned, when sending the request.
+  *   effect returned, when sending the request.
   */
 case class RequestOptions(
     followRedirects: Boolean,
