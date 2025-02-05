2e5c56087c5a595739d3b2b685502c16c9b34f53
adamw <adam@warski.org>
2025-02-05 19:34:12 +0100

    Uncomment examples

--- examples/src/main/scala/sttp/client4/examples/testing/TestEndpointMultipleQueryParameters.scala
+++ examples/src/main/scala/sttp/client4/examples/testing/TestEndpointMultipleQueryParameters.scala
@@ -7,25 +7,25 @@ package sttp.client4.examples.testing
 import sttp.client4.*
 import sttp.client4.testing.*
 
-@main def testEndpointMultipleQueryParameters(): Unit = ???
-// val backend = SyncBackendStub
-//   .whenRequestMatches(_.uri.paramsMap.contains("filter"))
-//   .thenRespondAdjust("Filtered")
-//   .whenRequestMatches(_.uri.path.contains("secret"))
-//   .thenRespondAdjust("42")
+@main def testEndpointMultipleQueryParameters(): Unit =
+  val backend = SyncBackendStub
+    .whenRequestMatches(_.uri.paramsMap.contains("filter"))
+    .thenRespondAdjust("Filtered")
+    .whenRequestMatches(_.uri.path.contains("secret"))
+    .thenRespondAdjust("42")
 
-// val parameters1 = Map("filter" -> "name=mary", "sort" -> "asc")
-// println(
-//   basicRequest
-//     .get(uri"http://example.org?search=true&$parameters1")
-//     .send(backend)
-//     .body
-// )
+  val parameters1 = Map("filter" -> "name=mary", "sort" -> "asc")
+  println(
+    basicRequest
+      .get(uri"http://example.org?search=true&$parameters1")
+      .send(backend)
+      .body
+  )
 
-// val parameters2 = Map("sort" -> "desc")
-// println(
-//   basicRequest
-//     .get(uri"http://example.org/secret/read?$parameters2")
-//     .send(backend)
-//     .body
-// )
+  val parameters2 = Map("sort" -> "desc")
+  println(
+    basicRequest
+      .get(uri"http://example.org/secret/read?$parameters2")
+      .send(backend)
+      .body
+  )
