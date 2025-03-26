18074e8df98de666a58ef2c81faade9df6c7fece
scala-steward <scala-steward>
2025-03-26 00:33:03 +0000

    Update client4:caching-backend, ... to 4.0.0-RC2

--- examples/src/main/scala/sttp/client4/examples/resilience/RetryZio.scala
+++ examples/src/main/scala/sttp/client4/examples/resilience/RetryZio.scala
@@ -1,6 +1,6 @@
 // {cat=Resilience; effects=ZIO; backend=HttpClient}: Retry sending a request using ZIO's retries
 
-//> using dep com.softwaremill.sttp.client4::zio:4.0.0-RC1
+//> using dep com.softwaremill.sttp.client4::zio:4.0.0-RC2
 
 package sttp.client4.examples.resilience
 
