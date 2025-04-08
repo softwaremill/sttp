f2cb8910a9790ad4d01a9d5c637bc0ac53e6ee95
adamw <adam@warski.org>
2025-04-08 08:53:14 +0200

    Fix compilation for 2.12

--- effects/cats/src/test/scala/sttp/client4/impl/cats/CatsRetryTest.scala
+++ effects/cats/src/test/scala/sttp/client4/impl/cats/CatsRetryTest.scala
@@ -13,7 +13,7 @@ import scala.util.Random
 trait CatsRetryTest extends AsyncFreeSpecLike { this: HttpTest[IO] =>
   private def retry[A](task: IO[A], delay: FiniteDuration, retries: Int): IO[A] =
     task
-      .onError(e => IO.delay(println(s"Received error: $e, retries left = $retries")))
+      .onError { case e => IO.delay(println(s"Received error: $e, retries left = $retries")) }
       .handleErrorWith { error =>
         if (retries > 0)
           IO.sleep(delay) *> retry(task, delay, retries - 1)
