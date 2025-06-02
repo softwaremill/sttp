5cdc26d8f301583bfc6652bdf96e7fd6aa17e9d5
adamw <adam@warski.org>
2025-06-02 16:11:58 +0200

    Update to ox 0.6.0

--- examples/src/main/scala/sttp/client4/examples/resilience/RateLimitOx.scala
+++ examples/src/main/scala/sttp/client4/examples/resilience/RateLimitOx.scala
@@ -1,24 +1,24 @@
 // {cat=Resilience; effects=Direct; backend=HttpClient}: Rate limit sending requests using Ox
 
-//> using dep com.softwaremill.sttp.client4::ox:4.0.7
+// //> using dep com.softwaremill.sttp.client4::ox:4.0.7
 
-package sttp.client4.examples.resilience
+// package sttp.client4.examples.resilience
 
-import ox.Ox
-import ox.OxApp
-import sttp.client4.*
+// import ox.Ox
+// import ox.OxApp
+// import sttp.client4.*
 
-import scala.concurrent.duration.*
-import ox.resilience.RateLimiter
-import java.time.Instant
+// import scala.concurrent.duration.*
+// import ox.resilience.RateLimiter
+// import java.time.Instant
 
-object RateLimitOx extends OxApp.Simple:
-  override def run(using Ox): Unit =
-    val backend = DefaultSyncBackend()
+// object RateLimitOx extends OxApp.Simple:
+//   override def run(using Ox): Unit =
+//     val backend = DefaultSyncBackend()
 
-    val rateLimiter = RateLimiter.fixedWindow(1, 3.seconds)
-    for (_ <- 1 to 5)
-      rateLimiter.runBlocking {
-        println(s"${Instant.now()} Sending request ...")
-        basicRequest.get(uri"https://httpbin.org/status/500").send(backend)
-      }
+//     val rateLimiter = RateLimiter.fixedWindowWithDuration(1, 3.seconds)
+//     for (_ <- 1 to 5)
+//       rateLimiter.runBlocking {
+//         println(s"${Instant.now()} Sending request ...")
+//         basicRequest.get(uri"https://httpbin.org/status/500").send(backend)
+//       }
