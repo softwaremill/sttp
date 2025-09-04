46f128d54cce8c77d15918f7abcc312d1a828dc1
Michal Pawlik <admin@michalp.net>
2025-09-04 17:55:54 +0200

    simplify tests

--- effects/cats/src/test/scalanative/sttp/client4/curl/cats/CurlCatsTestBase.scala
+++ effects/cats/src/test/scalanative/sttp/client4/curl/cats/CurlCatsTestBase.scala
@@ -3,8 +3,8 @@ package sttp.client4.curl.cats
 import cats.effect.IO
 import org.scalatest.Suite
 import sttp.client4.Backend
-import sttp.client4.impl.cats.{CatsTestBase, TestIODispatcher}
+import sttp.client4.impl.cats.CatsTestBase
 
-trait CurlCatsTestBase extends CatsTestBase with TestIODispatcher { this: Suite =>
+trait CurlCatsTestBase extends CatsTestBase { this: Suite =>
   implicit val backend: Backend[IO] = CurlCatsBackend[IO]()
 }
