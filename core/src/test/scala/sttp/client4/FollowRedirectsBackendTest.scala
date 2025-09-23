cff184fd21707ec17fd860d2e3d67e0c2f53c0e4
Zerkath <24889977+Zerkath@users.noreply.github.com>
2025-09-23 20:38:07 +0300

    test: redirection cookie behavior ignoring empty sensitive headers set
    Redirects do not apply set-cookie in the request chain meaning
    multistep request chains that track state will be missing cookies in the
    middle of redirection, only real alternative is manual control
    
    After redirection chain completes any set-cookies that started or were
    in the middle of the request chain get ignored. Only the last responses set-cookie's
    will be applied. It is possible to collect the cookies / headers from
    response.history, but adds complexity.

--- core/src/test/scala/sttp/client4/FollowRedirectsBackendTest.scala
+++ core/src/test/scala/sttp/client4/FollowRedirectsBackendTest.scala
@@ -7,6 +7,8 @@ import sttp.client4.testing.{BackendStub, ResponseStub}
 import sttp.client4.wrappers.{FollowRedirectsBackend, FollowRedirectsConfig}
 import sttp.model.internal.Rfc3986
 import sttp.model.{Header, StatusCode, Uri}
+import sttp.model.headers.CookieWithMeta
+import sttp.model.Method
 
 class FollowRedirectsBackendTest extends AnyFunSuite with Matchers with EitherValues {
   val testData = List(
@@ -53,4 +55,38 @@ class FollowRedirectsBackendTest extends AnyFunSuite with Matchers with EitherVa
     result.body.value shouldBe "All good!"
   }
 
+  test("should pass cookies during redirections") {
+
+    val url0 = uri"https://example.com/0"
+    val url1 = uri"https://example.com/1"
+
+    val response0 = ResponseStub
+      .adjust(
+        "",
+        StatusCode.Found,
+        Vector(
+          Header.location(url1),
+          Header.setCookie(CookieWithMeta("session", "one"))
+        )
+      )
+
+    val stub0 = BackendStub.synchronous
+      .whenRequestMatches { r => r.uri == url0 && r.method == Method.POST }
+      .thenRespond(response0)
+      .whenRequestMatches { r => r.uri == url1 && r.method == Method.GET && r.unsafeCookies.isEmpty }
+      .thenRespondUnauthorized()
+      .whenRequestMatches { r =>
+        r.uri == url1 && r.method == Method.GET && r.unsafeCookies.map(_.name).contains("session")
+      }
+      .thenRespondOk()
+      .whenAnyRequest
+      .thenRespondNotFound()
+
+    val redirectsBackend =
+      wrappers.FollowRedirectsBackend(stub0, config = FollowRedirectsConfig(sensitiveHeaders = Set.empty))
+    val result = basicRequest.redirectToGet(true).post(url0).send(redirectsBackend)
+
+    result.unsafeCookies.size shouldEqual 1
+    result.code shouldBe StatusCode.Ok
+  }
 }
