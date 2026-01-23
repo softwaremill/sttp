b3e9b9bb9a03c01d1322e6854b005f20ce5bc557
scala-steward <scala-steward>
2026-01-23 00:42:47 +0000

    Update client4:caching-backend, ... to 4.0.15

--- examples/src/main/scala/sttp/client4/examples/wrapper/retryingBackend.scala
+++ examples/src/main/scala/sttp/client4/examples/wrapper/retryingBackend.scala
@@ -1,6 +1,6 @@
 // {cat=Backend wrapper; effects=Synchronous; backend=HttpClient}: Simple retrying backend wrapper
 
-//> using dep com.softwaremill.sttp.client4::core:4.0.14
+//> using dep com.softwaremill.sttp.client4::core:4.0.15
 
 package sttp.client4.examples.wrapper
 
