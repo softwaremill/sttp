7900d15ac764ab9fe9ab7c8dae8d06f1b779ad74
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2026-02-16 16:47:42 +0100

    Added streaming support to scala-native curl-based sync backend (#2807)
    Added sync streaming support to scala-native curl-based sync backend
    using curl_multi api family
    
    Before submitting pull request:
    - [X] Check if the project compiles by running `sbt compile`
    - [X] Verify docs compilation by running `sbt compileDocs`
    - [ ] Check if tests pass by running `sbt test`
    - [X] Format code by running `sbt scalafmt`

--- core/src/main/resources/scala-native/ffi.c
+++ core/src/main/resources/scala-native/ffi.c
@@ -8,4 +8,18 @@ const char* sttp_curl_get_version() {
     return curl_version_info(CURLVERSION_NOW)->version;
 }
 int sttp_curl_getinfo_pointer(CURL *curl, CURLINFO info, void* arg) {return curl_easy_getinfo(curl, info, arg); }
+
+/* curl_multi wrappers */
+/* Most curl_multi functions are called directly via @extern bindings.
+   This wrapper exists because CURLMsg contains a union that can't be
+   safely represented in Scala Native's type system. */
+int sttp_curl_multi_info_read_result(CURLM *multi, CURL **easy_out) {
+    int msgs_in_queue;
+    CURLMsg *msg = curl_multi_info_read(multi, &msgs_in_queue);
+    if (msg && msg->msg == CURLMSG_DONE) {
+        if (easy_out) *easy_out = msg->easy_handle;
+        return (int)msg->data.result;
+    }
+    return -1; /* no completed transfer */
+}
 #endif
