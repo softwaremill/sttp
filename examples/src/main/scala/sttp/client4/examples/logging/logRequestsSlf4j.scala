80de1e4b06d1e40a5c1465ed75c43db455008557
scala-steward <scala-steward>
2026-02-17 00:47:35 +0000

    Update logback-classic to 1.5.32

--- examples/src/main/scala/sttp/client4/examples/logging/logRequestsSlf4j.scala
+++ examples/src/main/scala/sttp/client4/examples/logging/logRequestsSlf4j.scala
@@ -3,7 +3,7 @@
 //> using dep com.softwaremill.sttp.client4::circe:4.0.15
 //> using dep com.softwaremill.sttp.client4::slf4j-backend:4.0.15
 //> using dep io.circe::circe-generic:0.14.15
-//> using dep ch.qos.logback:logback-classic:1.5.31
+//> using dep ch.qos.logback:logback-classic:1.5.32
 
 package sttp.client4.examples.logging
 
