f5ba4bb01565f4d65e7c83cffb8edf1119821e67
scala-steward <scala-steward>
2026-03-31 00:51:54 +0000

    Update client4:caching-backend, ... to 4.0.21

--- examples/src/main/scala/sttp/client4/examples/PostFormSynchronous.scala
+++ examples/src/main/scala/sttp/client4/examples/PostFormSynchronous.scala
@@ -1,6 +1,6 @@
 // {cat=Hello, World!; effects=Direct; backend=HttpClient}: POST form data
 
-//> using dep com.softwaremill.sttp.client4::core:4.0.19
+//> using dep com.softwaremill.sttp.client4::core:4.0.21
 
 package sttp.client4.examples
 
