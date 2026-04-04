811e4cdee8be4c0405159fc372ea2bacbe86827a
scala-steward <scala-steward>
2026-04-04 00:47:00 +0000

    Update jedis to 7.4.1

--- examples/src/main/scala/sttp/client4/examples/wrapper/redisCachingBackend.scala
+++ examples/src/main/scala/sttp/client4/examples/wrapper/redisCachingBackend.scala
@@ -2,7 +2,7 @@
 
 //> using dep com.softwaremill.sttp.client4::core:4.0.21
 //> using dep com.softwaremill.sttp.client4::caching-backend:4.0.21
-//> using dep redis.clients:jedis:7.4.0
+//> using dep redis.clients:jedis:7.4.1
 //> using dep ch.qos.logback:logback-classic:1.5.15
 
 package sttp.client4.examples.wrapper
