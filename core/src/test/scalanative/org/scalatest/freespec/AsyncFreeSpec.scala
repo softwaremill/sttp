c3787762d6c7396f867b89594d2ee2b0926a54ee
Maksym Ochenashko <maxochenashko@gmail.com>
2026-04-11 19:13:11 +0100

    Scala Native: add ExecutionContext to the AsyncFreeSpec

--- core/src/test/scalanative/org/scalatest/freespec/AsyncFreeSpec.scala
+++ core/src/test/scalanative/org/scalatest/freespec/AsyncFreeSpec.scala
@@ -1,4 +1,8 @@
 package org.scalatest.freespec
 
+import scala.concurrent.ExecutionContext
+
 // added only to make the tests compile, since it's used in shared tests
-trait AsyncFreeSpec extends AnyFreeSpec
+trait AsyncFreeSpec extends AnyFreeSpec {
+  def executionContext: ExecutionContext = ExecutionContext.global
+}
