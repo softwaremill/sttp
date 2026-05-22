a378bb8b8d0fe1ff2259ddecb8540a43f76ee3c9
softwaremill-ci <ci@softwaremill.com>
2026-05-22 03:23:54 +0200

    Update caching-backend, cats, circe, fs2, ... to 4.0.24 (#2888)

--- examples-ce2/src/main/scala/sttp/client4/examples/PostSerializeJsonMonixHttpClientCirce.scala
+++ examples-ce2/src/main/scala/sttp/client4/examples/PostSerializeJsonMonixHttpClientCirce.scala
@@ -2,7 +2,7 @@
 
 //> using scala 2.13
 //> using dep com.softwaremill.sttp.client4::monix:4.0.23
-//> using dep com.softwaremill.sttp.client4::circe:4.0.23
+//> using dep com.softwaremill.sttp.client4::circe:4.0.24
 //> using dep io.circe::circe-generic:0.14.15
 
 package sttp.client4.examples
