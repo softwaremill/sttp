a170d3710d1de4d2f30cb1abf38dd61f16be1cd6
adamw <adam@warski.org>
2026-05-28 16:20:46 +0000

    task: Render multipart filename, content-type, and per-part headers in toCurl

--- core/src/test/scalajvm/sttp/client4/ToCurlConverterTestExtension.scala
+++ core/src/test/scalajvm/sttp/client4/ToCurlConverterTestExtension.scala
@@ -13,7 +13,20 @@ trait ToCurlConverterTestExtension { suit: Suite with AnyFlatSpec with Matchers 
       .multipartBody(multipartSttpFile("upload", SttpFile.fromPath(new File("myDataSet").toPath)))
       .post(uri"http://localhost")
       .toCurl should include(
-      """--form 'upload=@myDataSet'"""
+      """--form 'upload=@myDataSet;filename=myDataSet;type=application/octet-stream'"""
+    )
+  }
+
+  it should "render multipart file part with overridden filename and content type" in {
+    basicRequest
+      .multipartBody(
+        multipartSttpFile("upload", SttpFile.fromPath(new File("data.bin").toPath))
+          .fileName("renamed.pdf")
+          .contentType("application/pdf")
+      )
+      .post(uri"http://localhost")
+      .toCurl should include(
+      """--form 'upload=@data.bin;filename=renamed.pdf;type=application/pdf'"""
     )
   }
 }
