90fa4e9c70617a487964616225012fc83a5606b9
softwaremill-ci <ci@softwaremill.com>
2026-09-25 09:52:38 +0200

    Update jedis to 8.0.1 (#2983)

--- examples/src/main/scala/sttp/client4/examples/wrapper/redisCachingBackend.scala
+++ examples/src/main/scala/sttp/client4/examples/wrapper/redisCachingBackend.scala
@@ -2,12 +2,12 @@
 
 //> using dep com.softwaremill.sttp.client4::core:4.0.26
 //> using dep com.softwaremill.sttp.client4::caching-backend:4.0.26
-//> using dep redis.clients:jedis:7.5.3
+//> using dep redis.clients:jedis:8.0.1
 //> using dep ch.qos.logback:logback-classic:1.5.15
 
 package sttp.client4.examples.wrapper
 
-import redis.clients.jedis.UnifiedJedis
+import redis.clients.jedis.{RedisClient, UnifiedJedis}
 import sttp.client4.*
 import sttp.client4.caching.Cache
 import sttp.client4.caching.CachingBackend
@@ -31,7 +31,7 @@ class RedisCache(jedis: UnifiedJedis) extends Cache[Identity]:
 
 @main def redisCachingBackend(): Unit =
   val backend: WebSocketSyncBackend =
-    CachingBackend(DefaultSyncBackend(), new RedisCache(new UnifiedJedis("redis://localhost:6379")))
+    CachingBackend(DefaultSyncBackend(), new RedisCache(RedisClient.create("redis://localhost:6379")))
 
   // returns a response with a max-age of 3 seconds
   val request = basicRequest.get(uri"https://httpbin.org/cache/3")
